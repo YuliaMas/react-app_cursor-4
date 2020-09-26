07ae8f650f4341d5f8854252d4c660c104589952
Yuliya <yulia.masechko@gmail.com>
2020-09-27 02:02:52 +0300

    Updates

---
+++ precache-manifest.12b1c4ade15247d84615050508ea6d94.js
@@ -0,0 +1,134 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "53186b146b850e8c4eb5336d3bbe8164",
+    "url": "/react-app_cursor-4/index.html"
+  },
+  {
+    "revision": "a580b0707d593575b162",
+    "url": "/react-app_cursor-4/static/css/main.30260da9.chunk.css"
+  },
+  {
+    "revision": "47a05df046f3c648d986",
+    "url": "/react-app_cursor-4/static/js/2.7ce4cc86.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/react-app_cursor-4/static/js/2.7ce4cc86.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a580b0707d593575b162",
+    "url": "/react-app_cursor-4/static/js/main.a64c5921.chunk.js"
+  },
+  {
+    "revision": "0589253bb0eac6eb9fe1",
+    "url": "/react-app_cursor-4/static/js/runtime-main.9fb37f95.js"
+  },
+  {
+    "revision": "c6226386cefbee0a33b8a7b5b396cf23",
+    "url": "/react-app_cursor-4/static/media/1.c6226386.jpg"
+  },
+  {
+    "revision": "f31ea55d1ba03788891d9c86e6229094",
+    "url": "/react-app_cursor-4/static/media/12.f31ea55d.jpg"
+  },
+  {
+    "revision": "4b77df604f058c0f061a0127f10fcb52",
+    "url": "/react-app_cursor-4/static/media/287_3008x2000_all-free-download.com_3476674.4b77df60.jpg"
+  },
+  {
+    "revision": "fd85c64f5b24c987fb095e946c18593d",
+    "url": "/react-app_cursor-4/static/media/GM_0002.fd85c64f.JPG"
+  },
+  {
+    "revision": "d043070659f9e3315247d7c109431806",
+    "url": "/react-app_cursor-4/static/media/autumn_background_208623.d0430706.jpg"
+  },
+  {
+    "revision": "89818f448fb003387e5f515a99a69387",
+    "url": "/react-app_cursor-4/static/media/autumn_in_the_world_of_mountains_513525.89818f44.jpg"
+  },
+  {
+    "revision": "8667c281eaa56eae0e6d480dcc3a7163",
+    "url": "/react-app_cursor-4/static/media/background_with_flower_and_butterfly_201511.8667c281.jpg"
+  },
+  {
+    "revision": "8cfeaa0d706e3de2bb090573070f4b63",
+    "url": "/react-app_cursor-4/static/media/blackberry_background_186066.8cfeaa0d.jpg"
+  },
+  {
+    "revision": "a363238cba498860acb20c7c487d63ee",
+    "url": "/react-app_cursor-4/static/media/blue_grapes_516586.a363238c.jpg"
+  },
+  {
+    "revision": "d87151d29ac177d15da795e3714161a6",
+    "url": "/react-app_cursor-4/static/media/comment.d87151d2.svg"
+  },
+  {
+    "revision": "bcc0ba3cdb5ef9ae0d18f742df6cae8d",
+    "url": "/react-app_cursor-4/static/media/conkers_leaf_182689.bcc0ba3c.jpg"
+  },
+  {
+    "revision": "372543a12a75dcfdb926a0a1fdfa1a12",
+    "url": "/react-app_cursor-4/static/media/dinner_204546.372543a1.jpg"
+  },
+  {
+    "revision": "d4521446c3a52ae3dabc8584f81299c2",
+    "url": "/react-app_cursor-4/static/media/down-arrow.d4521446.svg"
+  },
+  {
+    "revision": "d388e00b977834e5ce314b17aa24ebc2",
+    "url": "/react-app_cursor-4/static/media/halloween_autumn_theme_192437.d388e00b.jpg"
+  },
+  {
+    "revision": "0c7b5ebd4af2efc0f4abc73ab25a8b21",
+    "url": "/react-app_cursor-4/static/media/heart.0c7b5ebd.svg"
+  },
+  {
+    "revision": "f3f8acb8e0ac940f8fea36943568a0a2",
+    "url": "/react-app_cursor-4/static/media/idyllic_beach_187063.f3f8acb8.jpg"
+  },
+  {
+    "revision": "6c004d3a9cd5141a2967f8286853574e",
+    "url": "/react-app_cursor-4/static/media/ivy_on_house_in_autumn_192599.6c004d3a.jpg"
+  },
+  {
+    "revision": "56b112351f007317a589ee1c87c7deab",
+    "url": "/react-app_cursor-4/static/media/leaves_decoration_background_187453.56b11235.jpg"
+  },
+  {
+    "revision": "7f325c6ce96c86fb9e04645a5fc7c2b3",
+    "url": "/react-app_cursor-4/static/media/never_give_up_514099.7f325c6c.jpg"
+  },
+  {
+    "revision": "598f504cf1c614e86da227848eafaa89",
+    "url": "/react-app_cursor-4/static/media/peeled_mandarin_187517.598f504c.jpg"
+  },
+  {
+    "revision": "a77fe8d322459cc02573d9067e8f3c65",
+    "url": "/react-app_cursor-4/static/media/prenter_down_low_514128.a77fe8d3.jpg"
+  },
+  {
+    "revision": "df7ee2c86d547e1206fd3dc424120b1f",
+    "url": "/react-app_cursor-4/static/media/raspberry_background_186058.df7ee2c8.jpg"
+  },
+  {
+    "revision": "e85ee3e0e5e550d695cac2c1de9055b1",
+    "url": "/react-app_cursor-4/static/media/retweet.e85ee3e0.svg"
+  },
+  {
+    "revision": "dfd3976ea15e4af844ba673fea834b32",
+    "url": "/react-app_cursor-4/static/media/share.dfd3976e.svg"
+  },
+  {
+    "revision": "1e7402782bcbe2dd24e0d4f76c76d7eb",
+    "url": "/react-app_cursor-4/static/media/warming_autumn_rays_513747.1e740278.jpg"
+  },
+  {
+    "revision": "baa3a5a6d57d5aec6a63a3f5d08631c3",
+    "url": "/react-app_cursor-4/static/media/warranty.baa3a5a6.svg"
+  },
+  {
+    "revision": "52e7e86e014ac79a84ce7e08bdf8388b",
+    "url": "/react-app_cursor-4/static/media/waterdrop_background_201564.52e7e86e.jpg"
+  }
+]);
